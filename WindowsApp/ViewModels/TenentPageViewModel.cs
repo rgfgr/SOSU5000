3be32214faccaa6598a52ae7a265d79eb4aa177e
nikl3009 <nikl3009@CV-BB-5483>
2022-11-09 14:48:01 +0100

    Added TenentPage and viewmodel

---
+++ WindowsApp/ViewModels/TenentPageViewModel.cs
@@ -0,0 +1,24 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsApp.ViewModels
+{
+    [QueryProperty("Tenent", "Tenent")]
+    public partial class TenentPageViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        Tenent tenent;
+
+        [RelayCommand]
+        async Task GoBack()
+        {
+            await Shell.Current.GoToAsync("..");
+        }
+    }
+}
