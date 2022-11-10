da5ace723f6943cf12c7b54bc3bc42a4c4d91c52
nikl3009 <nikl3009@CV-BB-5483>
2022-11-10 12:55:26 +0100

    Added And made functionale

---
+++ WindowsApp/ViewModels/AssignmentPageViewModel.cs
@@ -0,0 +1,33 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WindowsApp.Services.Interfaces;
+
+namespace WindowsApp.ViewModels
+{
+    [QueryProperty("Id", "Id")]
+    public partial class AssignmentPageViewModel : ObservableObject
+    {
+        IAssignmentService assignmentService;
+
+        public AssignmentPageViewModel(IAssignmentService assignmentService)
+        {
+            this.assignmentService = assignmentService;
+        }
+
+        [ObservableProperty]
+        Assignment assignment;
+
+        [ObservableProperty]
+        int id;
+
+        internal async Task Loaded()
+        {
+            Assignment = await assignmentService.GetAssignment(Id);
+        }
+    }
+}
